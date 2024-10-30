2dd51cc24aa1444ba0bf1b004779cfc463e489c9
ikazbat <drisskazbat01@gmail.com>
2024-10-30 14:01:43 +0100

    add

--- controllers/categories.go
+++ controllers/categories.go
@@ -7,25 +7,32 @@ import (
 	"forum/utils"
 )
 
-// ;
-// INSERT INTO categories (categori,post_id)VALUES()
-func CreateCategories(categories models.Categories) error {
+func CreateCategories(categories models.Categories) (bool, error) {
 	var count int
 	err := utils.DataBase.QueryRow("SELECT COUNT(*) FROM categories WHERE categori = ? nameCategori", categories.Categori).Scan(&count)
 	if err != nil {
-		return err
+		return false, err
 	}
 	if count > 0 {
-		return errors.New("categori already exist")
+		return false, errors.New("categori already exist")
 	}
 	stmt, err := utils.DataBase.Prepare("INSERT INTO categories (categori,post_id) VALUES(?,?)")
 	if err != nil {
-		return err
+		return false, err
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(categories.Categori, categories.PostId)
 	if err != nil {
-		return err
+		return false, err
 	}
-	return nil
+	return true, nil
+}
+
+func DisplayCategories(categories models.Categories) (string, error) {
+	var resalt string
+	err := utils.DataBase.QueryRow("SELECT categori FROM categories").Scan(&resalt)
+	if err != nil {
+		return "", err
+	}
+	return resalt, nil
 }
