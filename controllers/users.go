4aafc9d5d02f37747c196c1cd479a7f1b41e80c1
kazbatdriss1 <kazbatdriss81@gmail.com>
2024-11-02 18:03:27 +0100

    add a Handler for create post and comment and reactions

--- controllers/users.go
+++ controllers/users.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"forum/models"
 	"forum/utils"
@@ -127,4 +128,74 @@ func GetSession(r *http.Request) (models.User, error) {
 	}
 
 	return user, nil
+}
+
+func CreatePost(postContent models.PostContent) error {
+	C_post, err := utils.DataBase.Prepare(`INSERT INTO posts(user_id, title, content, image_url, created_at) VALUES(?, ?, ?, ?, ?)`)
+	if err != nil {
+		return fmt.Errorf("error preparing statement: %w", err)
+	}
+	defer C_post.Close()
+
+	res, err := C_post.Exec(postContent.User_id, postContent.Title, postContent.Content, postContent.Image_url, postContent.Created_at)
+	if err != nil {
+		return err
+	}
+
+	// Get the ID of the newly created post
+	postID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	C_postCategories, err := utils.DataBase.Prepare(`INSERT INTO post_categories(post_id, category_id) VALUES(?, ?)`)
+	if err != nil {
+		return fmt.Errorf("error preparing statement for post_categories: %w", err)
+	}
+	defer C_postCategories.Close()
+
+	for _, categoryID := range postContent.Category_id {
+		Category_id, _ := strconv.Atoi(categoryID)
+		if _, err := C_postCategories.Exec(postID, Category_id); err != nil {
+			return fmt.Errorf("error linking post to category %s: %w", categoryID, err)
+		}
+	}
+	return nil
+}
+
+func CreateComments(commentContent models.Comments) error {
+	C_comment, err := utils.DataBase.Prepare(`INSERT INTO comments(post_id, user_id, content, created_at) VALUES(?, ?, ?, ?)`)
+	if err != nil {
+		return fmt.Errorf("error preparing statement: %w", err)
+	}
+
+	defer C_comment.Close()
+
+	if _, err = C_comment.Exec(commentContent.Post_id, commentContent.User_id, commentContent.Content, commentContent.Created_at); err != nil {
+		return fmt.Errorf("error executing statement: %w", err)
+	}
+
+	return nil
+}
+
+func CreateReaction(reactions models.Reactions) error {
+	var query string
+	var id int
+	isPost := reactions.Post_id != 0
+	if isPost {
+		query = `INSERT INTO reactions (user_id, post_id, is_like, created_at) VALUES (?, ?, ?, ?)`
+		id = reactions.Post_id
+	} else {
+		query = `INSERT INTO reactions (user_id, comment_id, is_like, created_at) VALUES (?, ?, ?, ?)`
+		id = reactions.Comment_id
+	}
+	C_reaction, err := utils.DataBase.Prepare(query)
+	if err != nil {
+		return fmt.Errorf("error executing statement: %w", err)
+	}
+	if _, err = C_reaction.Exec(reactions.User_id, id, reactions.Is_like, reactions.Created_at); err != nil {
+		return fmt.Errorf("error executing statement: %w", err)
+	}
+
+	return nil
 }
--- models/user.go
+++ models/user.go
@@ -14,3 +14,25 @@ type Session struct {
 	Username  string
 	ExpiresAt time.Time
 }
+type PostContent struct {
+	User_id     int
+	Title       string
+	Content     string
+	Category_id []string
+	Image_url   string
+	Created_at  string
+}
+type Comments struct {
+	Post_id    int
+	User_id    int
+	Content    string
+	Created_at string
+}
+
+type Reactions struct {
+	User_id    int
+	Post_id    int
+	Comment_id int
+	Is_like    bool
+	Created_at string
+}
