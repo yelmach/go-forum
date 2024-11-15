5eabb4a57e54a4fe3d27ab30efaff276a5823f29
yassinalmach <yassineelmach0@gmail.com>
2024-11-15 19:14:07 +0100

    add description to func

--- handlers/middleware/auth.go
+++ handlers/middleware/auth.go
@@ -8,40 +8,40 @@ import (
 	"forum/handlers"
 	"forum/utils"
 )
-
+// Middleware allows only users that authenticated to use next handler(add reaction, comment or post)
 func Middleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		is_valid := false
 		cookie_session, err := r.Cookie("session_id")
 		if err != nil {
-			handlers.ErrorHandler(w, r, http.StatusBadRequest)
+			utils.ResponseJSON(w, utils.Resp{Msg: "unauthorized user", Code: http.StatusUnauthorized})
 			return
 		}
-
 		session_id := cookie_session.Value
+		
 		cookie_user, err := r.Cookie("user_id")
 		if err != nil {
-			handlers.ErrorHandler(w, r, http.StatusBadRequest)
+			utils.ResponseJSON(w, utils.Resp{Msg: "unauthorized user", Code: http.StatusUnauthorized})
 			return
 		}
 
 		user_id, err := strconv.Atoi(cookie_user.Value)
 		if err != nil {
-			handlers.ErrorHandler(w, r, http.StatusBadRequest)
+			utils.ResponseJSON(w, utils.Resp{Msg: "unauthorized user", Code: http.StatusUnauthorized})
 			return
 		}
 
 		cookie_username, err := r.Cookie("username")
 		if err != nil {
-			handlers.ErrorHandler(w, r, http.StatusBadRequest)
+			utils.ResponseJSON(w, utils.Resp{Msg: "unauthorized user", Code: http.StatusUnauthorized})
 			return
 		}
 
 		user_username := cookie_username.Value
-		if err := database.DataBase.QueryRow(`SELECT EXISTS(SELECT * FROM sessions JOIN users ON sessions.user_id = users.id WHERE session_id = ? AND user_id = ? AND users.username = ? )`, session_id, user_id, user_username).Scan(&is_valid); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		query := `SELECT EXISTS(SELECT * FROM sessions JOIN users ON sessions.user_id = users.id WHERE session_id = ? AND user_id = ? AND users.username = ? )`
+		if err := database.DataBase.QueryRow(query, session_id, user_id, user_username).Scan(&is_valid); err != nil {
+			handlers.ErrorHandler(w, r, http.StatusInternalServerError)
 		}
-		
 		if !is_valid {
 			utils.DeleteCookie(w, "session_id")
 			utils.DeleteCookie(w, "user_id")
