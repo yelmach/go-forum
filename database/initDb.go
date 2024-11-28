5788a9186598a42c1ad8eb2c9ebda1fc4b461a47
yassinalmach <yassineelmach0@gmail.com>
2024-11-28 16:36:08 +0100

    remove image section in schema.sql

--- database/initDb.go
+++ database/initDb.go
@@ -9,6 +9,7 @@ import (
 
 var DataBase *sql.DB
 
+// InitDb opens an SQLITE database and creates tables
 func InitDb() error {
 	// Initialize database connection
 	db, err := sql.Open("sqlite3", "./database.db")
@@ -16,15 +17,16 @@ func InitDb() error {
 		return err
 	}
 
-	if err = db.Ping(); err != nil {
+	if err := db.Ping(); err != nil {
 		return err
 	}
+
 	query, err := os.ReadFile("./database/schema.sql")
 	if err != nil {
 		return err
 	}
 
-	if _, err = db.Exec(string(query)); err != nil {
+	if _, err := db.Exec(string(query)); err != nil {
 		return err
 	}
 
