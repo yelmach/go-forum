266fc64c08d08e6217b16384a067b7bd1bcb0d5f
kazbatdriss1 <kazbatdriss81@gmail.com>
2024-12-04 15:53:43 +0100

    fix a error of api

--- routers/api.go
+++ routers/api.go
@@ -3,11 +3,20 @@ package routers
 import (
 	"net/http"
 
+	"forum/handlers"
 	"forum/handlers/api"
 )
+
 // Setup Api Routes
 func SetupApiRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "api/posts" {
+			handlers.ErrorHandler(w, r, http.StatusNotFound)
+			return
+		}
+	})
 
 	mux.HandleFunc("/api/posts", api.LoadData)
 	mux.HandleFunc("/api/posts/{id}", api.LoadPostData)
