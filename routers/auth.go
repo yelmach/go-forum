e1f87abf9b8040977cf43a56215d398249cc03b9
kazbatdriss1 <kazbatdriss81@gmail.com>
2024-12-09 18:12:53 +0100

    add error to api if summe path not exist

--- routers/auth.go
+++ routers/auth.go
@@ -3,12 +3,17 @@ package routers
 import (
 	"net/http"
 
+	"forum/handlers"
 	"forum/handlers/auth"
 )
 
 // Setup Auth Routes
 func SetupAuthRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/auth/", func(w http.ResponseWriter, r *http.Request) {
+		handlers.ErrorHandler(w, r, http.StatusNotFound)
+	})
 
 	mux.HandleFunc("/auth/register", auth.RegisterUser)
 	mux.HandleFunc("/auth/login", auth.LoginUser)
