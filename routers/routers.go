11ece3abfc84cae72e8d2bbfa01dc27aeaaf1e19
kazbatdriss1 <kazbatdriss81@gmail.com>
2024-12-14 12:23:55 +0100

    appele the func

--- routers/routers.go
+++ routers/routers.go
@@ -1,9 +1,14 @@
 package routers
 
-import "net/http"
+import (
+	"net/http"
+
+	"forum/controllers"
+)
 
 // SetupRoutes initialize all endpoints of forum project
 func SetupRoutes(rootMux *http.ServeMux) {
+	controllers.CreateCategories()
 	rootMux.Handle("/assets/", SetupAssetsRoutes())
 	rootMux.Handle("/", SetupHomeRoutes())
 	rootMux.Handle("/api/", SetupApiRoutes())
